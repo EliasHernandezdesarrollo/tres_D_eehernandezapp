1dcc3999b088129708df305ca78153e599529102
EliasHernandezdesarrollo <eehernandez@est.istg.edu.ec>
2023-03-26 17:29:43 -0500

    Se agrego los modelos

--- tres_D_eehernandezapp/blogApp/models.py
+++ tres_D_eehernandezapp/blogApp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+Usuario_Blog = settings.AUTH_USER_MODEL
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50)
+    abreviatura = models.CharField(max_length=4)
+
+    @classmethod
+    def get_default_values(cls):
+        return Pais.objects.get(id=1)
+
+    def __str__(self):
+        return self.nombre
+
+class Tag(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+class Posts(models.Model):
+    user = models.ForeignKey(Usuario_Blog, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    tag = models.ManyToManyField(Tag)
+    contenido = models.TextField()
+    fecha_de_creacion = models.DateTimeField(auto_now_add=True)
+    actualizo_el = models.DateField(null=True)
+
+    class Meta:
+        ordering = ['-fecha_de_creacion']
+
+    def __str__(self):
+        return self.titulo
--- tres_D_eehernandezapp/loginApp/models.py
+++ tres_D_eehernandezapp/loginApp/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from tres_D_eehernandezapp.blogApp.models import Pais
 
 # Create your models here.
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, nombres, cedula, pais, password=None):
+        if not email:
+            raise ValueError('el usuario debe tener un email.')
+
+        if not cedula:
+            raise ValueError('el usuario debe tener una cedula.')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nombres=nombres,
+            cedula=cedula,
+            pais=pais
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, nombres, cedula, password):
+        user = self.create_user(
+            email,
+            password=password,
+            nombres=nombres,
+            cedula=cedula,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nombres, cedula, password):
+        user = self.create_user(
+            email,
+            password=password,
+            nombres=nombres,
+            cedula=cedula,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class Usuario(AbstractBaseUser):
+    nombres = models.CharField(max_length=255)
+    cedula = models.CharField(max_length=10, unique=True)
+    # email = models.CharField(max_length=255, verbose_name='email', unique=True)
+    email = models.EmailField(("email address: "), unique=True)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['cedula', 'nombres']
+
+    objects = UsuarioManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.staff
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+
+    class Meta:
+        db_table = 'User'
+        managed = True
