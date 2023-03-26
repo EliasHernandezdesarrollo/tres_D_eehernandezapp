b8b859c4b4d3981558e1f071ecfb2c356da1cdf9
EliasHernandezdesarrollo <eehernandez@est.istg.edu.ec>
2023-03-26 17:49:24 -0500

    Cambios en setting.py

--- tres_D_eehernandezapp/tres_D_eehernandezapp/settings.py
+++ tres_D_eehernandezapp/tres_D_eehernandezapp/settings.py
@@ -114,6 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'miblog.Usuario'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -124,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/home/'
+LOGOUT_REDIRECT_URL = '/login/'
